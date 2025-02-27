1e27a648bf2f7163b22c8f82b6ce4dd7e247877a
fpsParser <mattserious2302@gmail.com>
2025-02-27 22:52:37 +0300

    стабильная

--- main.js
+++ main.js
@@ -1,34 +1,30 @@
 const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
 
-// Функция для создания окна приложения
 function createWindow() {
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        minWidth: 380,  // Минимальная ширина окна
+        minHeight: 510, // Минимальная высота окна
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
         },
-        // Убираем меню
         autoHideMenuBar: true,
     });
 
-    // Загружаем index.html напрямую
     mainWindow.loadFile(path.join(__dirname, 'public', 'index.html'));
 
-    // Убираем DevTools (инструменты разработчика)
-    // mainWindow.webContents.openDevTools(); // Закомментируйте эту строку
+    // Раскомментируй, если нужно открыть DevTools
+    // mainWindow.webContents.openDevTools();
 }
 
-// Убираем стандартное меню
 Menu.setApplicationMenu(null);
 
-// Когда приложение готово, создаем окно
 app.whenReady().then(() => {
     createWindow();
 
-    // Для macOS: если окно закрыто, но приложение ещё работает, создаем новое окно
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
             createWindow();
@@ -36,7 +32,6 @@ app.whenReady().then(() => {
     });
 });
 
-// Закрываем приложение, если все окна закрыты (кроме macOS)
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
